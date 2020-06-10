9b4a642ac5ac3394930b3c9503ae995118ce0e58
thinkpowerjerryyang <66706995+thinkpowerjerryyang@users.noreply.github.com>
2020-06-10 09:18:36 +0800

    20200610
    初始化

---
+++ CathayCINS/Web/js/custom/QueriesHelper30f4.js
@@ -0,0 +1,25 @@
+﻿
+//將外站傳來的白名單Query帶至所有<a>上
+$(function () {
+    if (!(typeof Querys === 'undefined')) {
+        $("a").each(function (index) {
+            if ($(this).prop('href')) {
+                var a = document.createElement('a');
+                a.href = $(this).prop('href');
+                var query = '';
+                var flag = true;
+                $.each(Querys, function (index, value) {
+                    if (flag && !a.search) {
+                        query += '?' + index + '=' + value;
+                        flag = false;
+                    }
+                    else {
+                        query += '&' + index + '=' + value;
+                        flag = false;
+                    }
+                });
+                $(this).attr('href', $(this).prop('href') + query);
+            }
+        });
+    }
+});
---
+++ CathayCINS/Web/js/custom/subMenuController30f4.js
@@ -0,0 +1,16 @@
+﻿$().ready(function () {
+    var width = $(window).width();
+    if (width <= 960) {
+        var navbarChildren = $('.navbar-right').children();
+        switch (navbarChildren.length) {
+            case 2:
+                break
+            case 1:
+                navbarChildren.attr('style', 'float: none; width: 100% !important;');
+                break;
+            case 0:
+                $('.subnav').hide();
+                break;
+        }
+    }
+})    
