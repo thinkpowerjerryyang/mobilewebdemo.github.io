443ba7ae7738c57b1533dda3d9add776de8fd9b1
Agnes Tseng <agnes.tseng@thinkpower-info.com>
2020-06-11 17:59:28 +0800

    index arrange

---
+++ www.cathay-ins.com.tw/CathayCINS/Web/js/index.js
@@ -0,0 +1,10 @@
+$(function () {
+  // meta
+  $("#meta").load("../../cathayins/personal/index-content/meta.html")
+  // Desktop
+  $("#desktop-header").load("../../cathayins/personal/index-content/desktop/desktop-header.html");
+  $("#desktop-footer").load("../../cathayins/personal/index-content/desktop/desktop-footer.html");
+  // Mobile
+  $("#mobile-header").load("../../cathayins/personal/index-content/mobile/mobile-header.html");
+  $("#mobile-footer").load("../../cathayins/personal/index-content/mobile/mobile-footer.html");
+});
