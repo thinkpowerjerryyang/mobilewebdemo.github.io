37b5d215f940166c3968aedec0c131720edca692
Agnes Tseng <agnes.tseng@thinkpower-info.com>
2020-06-12 17:53:46 +0800

    資料整理

---
+++ www.cathay-ins.com.tw/cathayins/index-content/index-1/index.js
@@ -0,0 +1,14 @@
+/** 讀取模板列表 */
+function loadTemplates(prefix = '../') {
+  loadContent('#sidebar', prefix, "sidebar.html")
+  loadContent('#desktop-header', prefix, "index-1/desktop/desktop-header.html")
+  loadContent('#desktop-footer', prefix, "index-1/desktop/desktop-footer.html")
+  loadContent('#mobile-header', prefix, "index-1/mobile/mobile-header.html")
+  loadContent('#mobile-footer', prefix, "index-1/mobile/mobile-footer.html")
+}
+
+/** 讀取模板 */
+function loadContent(selector, prefix, template) {
+  const path = prefix + 'index-content/' + template
+  $(selector).load(path)
+}
